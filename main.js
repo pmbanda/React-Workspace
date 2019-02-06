089efc11ae3d0b8891233645360d15e79f6a3f4c
Peter Eugene Mbanda <pmbandaft@gmail.com>
2019-02-06 15:07:30 -0600

    React setup files

---
+++ main.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow(){
+	// create the browser window
+	let win = new BrowserWindow({ width: 800, height: 600})
+
+	// load index.html file to the app
+	win.loadFile('index.html')
+
+}
+
+app.on('ready', createWindow)
+
+
